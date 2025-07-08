f5d18964f9b00c1e542e01094e1e340e3abfb9e8
tstohn <tim.stohn@yahoo.com>
2025-07-08 13:02:31 +0200

    change of script to count ESGI barcodes

--- SIGNALseq_Analysis/scripts/countBarcodesESGI.R
+++ SIGNALseq_Analysis/scripts/countBarcodesESGI.R
@@ -1,4 +1,4 @@
-# your_script.R
+# count barcode occurences in RNA data for ESGI in the demultiplexed output file
 
 library(tidyverse)
 library(tidyverse)
@@ -8,7 +8,7 @@ library(Biostrings)
 library(ggplot2)
 library(data.table)
 
-ESGI_RAW <- read_tsv("/DATA/t.stohn/analyses_ezgi/SIGNALseq_Analysis/output/ezgi_june25/RNA.tsv")
+ESGI_RAW <- read_tsv("/DATA/t.stohn/analyses_ezgi/SIGNALseq_Analysis/output/ESGI_RNA/RNA.tsv")
 ESGI_RAW$scID <- paste0(ESGI_RAW$`BC1.txt`, ESGI_RAW$`BC2.txt...4`, ESGI_RAW$`BC2.txt...6`)
 ESGI_RAW$BC1 <- ESGI_RAW$BC1.txt
 ESGI_RAW$BC2 <- ESGI_RAW$BC2.txt...4
@@ -18,7 +18,7 @@ ESGI_RAW <- ESGI_RAW %>%
   mutate(BC2 = as.character(reverseComplement(DNAStringSet(BC2)))) %>%
   mutate(BC3 = as.character(reverseComplement(DNAStringSet(BC3))))
 #assign the merge-barcodes
-convert_df <- read_tsv("/DATA/t.stohn/analyses_ezgi/SIGNALseq_Analysis/background_data/formatForEzgi_files/split_seqv2_rt_bc_sharing.tsv")
+convert_df <- read_tsv("/DATA/t.stohn/analyses_ezgi/SIGNALseq_Analysis/background_data/ESGI_files/split_seqv2_rt_bc_sharing.tsv")
 ESGI_RAW <- ESGI_RAW %>%
   left_join(convert_df, by = c("BC1" = "BARCODE"))
 ESGI_RAW <- ESGI_RAW %>%
@@ -38,4 +38,4 @@ ESGI <- ESGI_RAW %>%
   unique()
 
 # Save the tibble as an RDS file
-saveRDS(ESGI, "esgi_data.rds")
+saveRDS(ESGI, "/DATA/t.stohn/analyses_ezgi/SIGNALseq_Analysis/output/ESGI_RNA/ESGI_BARCODES.rds")
